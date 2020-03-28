1ada42612380cf744911fccd9ad0a8c7897e6521
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 21:59:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.join("")
+  array.each do |string|
+    string.size.sort do |a,b|
+      a <=> b
+    end
+  end
+end
+
+def swap_elements(array)
+  array.sort do |b,c|
+    c <=> b
+  end
+end
