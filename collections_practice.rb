8ea6a510786c130033e67c50bd0dda99f5f057d3
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 22:55:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,5 +42,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|string,index| string[index] == 1 ? string : string << "s"}
+  array.each_with_index.collect {|string,index| index == 1 ? string : string << "s"}
 end
