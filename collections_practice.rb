88fde4eae9e8a27e2c569661bef990330f7951db
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 22:11:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,7 +18,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[1], array[2] = array[2],array[1]
+  array[0], array[1], array[2] = array[0], array[2],array[1]
 end
 
 def reverse_array(array)
