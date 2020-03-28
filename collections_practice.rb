269e52df9e9571fbbdb15da153c22f136b00f3fb
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 22:55:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
 
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2],array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject {|sum,integer| sum + integer}
+end
+
+def add_s(array)
+  array.each_with_index.collect {|string,index| index == 1 ? string : string << "s"}
+end
